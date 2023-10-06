429c440932c5d74c53132b0a2d1c3212dfe7bb82
darcierre <darrnurse@gmail.com>
2023-10-06 17:56:29 -0400

    Filter sidebar added

--- src/pages/AfterSearch.js
+++ src/pages/AfterSearch.js
@@ -1,11 +1,20 @@
 import Image from 'next/image'
 import Header from '../components/Header.js';
-
+import Filters from "../components/Filters";
 
 export default function AfterSearch() {
     return (
         <>
-        <Header page="AfterSearch"/>
+          <div>
+            <Header page="AfterSearch"/>
+          </div>
+
+          <div className={"m-5 font-trend"}>
+            <button className={"rounded-3xl bg-coral p-2 text-white"}>Generate new locations</button>
+            <Filters text={["Length of Trip", "Hours", "Days", "Weeks"]}/>
+            <Filters text={["Price", "Low to High", "High to Low"]}/>
+            <button className={"rounded-3xl bg-purple-500 p-2 text-white"}>Use This Itinerary</button>
+          </div>
 
         </>
     )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 import '../../styles/globals.css';  // Adjust the path as needed
 import '../../styles/Slideshow.css';
+import '../../styles/Filter.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
