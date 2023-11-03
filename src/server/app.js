0ce5755628e60b4c719f8c55786ad9c11f5968cd
silvertoothGeek0 <bbvvleonardwolf@gmail.com>
2023-11-03 17:27:41 -0400

    minimal update of the .gitignore file and fixing something in the server

--- src/server/app.js
+++ src/server/app.js
@@ -4,7 +4,7 @@ const http = require("http");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
-const port = process.env.SERVER_PORT || "8080";//set the port
+const port = process.env.SERVER_PORT || "3060";//set the port
 const path = require("path");
 
 
