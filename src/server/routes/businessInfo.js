d6c1d998f7673ae2300f8d3aaee98ee79372110c
silvertoothGeek0 <bbvvleonardwolf@gmail.com>
2023-11-26 22:40:12 -0500

    minimal updates to back-end

--- src/server/routes/businessInfo.js
+++ src/server/routes/businessInfo.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pgp = require("pg-promise")();//needs to be like this as we are initializing without options
 require("dotenv").config({ path: `${process.cwd()}/.env`}); //get the configs from the .env file
-const db = pgp(process.env.DB_CONNECT_STRING);//we need some sort of connection as the parameter 
+const db = require('../Database/newDBConnection')
 
 router.get('/', (req, res) => {
     db.any("SELECT * FROM \"Business\" WHERE id=1")
