85daae6d3ed547e0a5fe2332962f40bc2de5eee3
darcierre <darrnurse@gmail.com>
2023-12-01 19:37:51 -0500

    .env dir

--- src/server/Database/models/Type.js
+++ src/server/Database/models/Type.js
@@ -1,5 +1,6 @@
 const {Sequelize, DataTypes, Model} = require("sequelize");
-require("dotenv").config({ path: `C:\\Users\\Darst\\WebstormProjects\\ctp-projects\\kaja-app-working-clone\\kaja-app\\.env`}); //get the configs from the .env file
+const path = require('path');
+require("dotenv").config({ path: path.join(__dirname, "../../../../.env")}); //get the configs from the .env file
 const sequelize = new Sequelize(`${process.env.DB_DIALECT}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER_HOST}:${process.env.DB_SERVER_PORT}/${process.env.DB_NAME}`);
 //we have to import the sequelize which will have a new instance used with the db...
 
