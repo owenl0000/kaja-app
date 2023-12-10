932b3ea4dbc5ef415cb89e05b9ec25fb13126c83
silvertoothGeek0 <bbvvleonardwolf@gmail.com>
2023-12-10 01:02:07 -0500

    pug updates

--- src/server/routes/addData.js
+++ src/server/routes/addData.js
@@ -1,13 +1,10 @@
 require("dotenv").config({ path: '../../.env'}) //configure the api environment
 const express = require("express");
 const router = express.Router();
-//const db = require("../Database/newDBConnection");
 const api = require('api')('@yelp-developers/v1.0#9nl412lo4fa3f9');
 const apiKey = `Bearer ${process.env.YELP_API_KEY}`;
 api.auth(apiKey); 
 const {db, models} = require("../Database/seed");
-
-
 
 
 
